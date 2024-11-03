bb387abdaac7230215cc91707e6fb3bf97a61f5d
matei.nedelschi <matei.nedelschi@nxp.com>
2024-11-03 20:31:45 +0200

    Removing node_modules from both folders

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
-const express = require("express")
+import express from 'express'
+import cors from 'cors'
 const app = express()
-const cors = require("cors")
+let router = express.Router()
+let port = process.env.PORT || 8080
 
 const corsOptions={
-    origin: "http://localhost:5173",
+    whiteList: "http://localhost:5173",
 }
 
 app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
 
 app.get("/api",(req,res)=>{
-    res.json({testNumbers: ["1","2","3"]})
+    res.send({"nice":[1,2,3]})
 })
 
-app.listen(8080,()=>{
+app.listen(port,()=>{
     console.log("Server started on port: 8080")
 })
